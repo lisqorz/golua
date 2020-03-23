1213f484c403010219e60b6edad915e5c29d34d6
lisqorz <mail@lishunqi.com>
2020-03-23 22:02:54 +0800

    'ch05'

---
+++ go/ch06/src/luago/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"golua/go/ch05/src/luago/api"
+	"golua/go/ch05/src/luago/state"
+)
+import . "luago/api"
+import _ "luago/binchunk"
+
+func main() {
+	ls:=state.New()
+	ls.PushInteger(1)
+	ls.PushString("2.0")
+	ls.PushString("3.0")
+	ls.PushNumber(4.0)
+	printStack(ls)
+
+	ls.Arith(api.LUA_OPADD)
+	printStack(ls)
+	ls.Arith(api.LUA_OPBNOT)
+	printStack(ls)
+	ls.Len(2)
+	printStack(ls)
+	ls.Concat(3)
+	printStack(ls)
+	ls.PushBoolean(ls.Compare(1, 2, api.LUA_OPEQ))
+	printStack(ls)
+
+}
+
+func printStack(ls LuaState) {
+	top := ls.GetTop()
+	for i := 1; i <= top; i++ {
+		t := ls.Type(i)
+		switch t {
+		case LUA_TBOOLEAN:
+			fmt.Printf("[%t]", ls.ToBoolean(i))
+		case LUA_TNUMBER:
+			fmt.Printf("[%g]", ls.ToNumber(i))
+		case LUA_TSTRING:
+			fmt.Printf("[%q]", ls.ToString(i))
+		default: // other values
+			fmt.Printf("[%s]", ls.TypeName(t))
+		}
+	}
+	fmt.Println()
+}
---
+++ go/ch06/src/luago/state/lua_state.go
@@ -0,0 +1,13 @@
+package state
+
+type luaState struct {
+	stack *luaStack
+
+}
+
+func New() *luaState {
+	return &luaState{
+		stack: newLuaStack(20),
+	}
+}
+
